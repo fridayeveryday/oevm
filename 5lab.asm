66abf175d38378a252e63fb215844e655f100fe9
Pavel <50109083+fridayeveryday@users.noreply.github.com>
2020-10-23 09:36:55 +0400

    Update 5lab.asm

--- 5lab.asm
+++ 5lab.asm
@@ -1,7 +1,6 @@
 %include "io.inc"
 
 section .data
-    msg db 'Hello, world!', 0
     mult dd 1, 2, 4, 8
     i dd 0
     n1 dd 0
@@ -12,31 +11,30 @@ section .text
 CMAIN:
     mov ebp, esp; for correct debugging
     
-    GET_DEC 4, eax
-    cmp eax, 2
-    je binary
-    
-    GET_DEC 4, eax
-    GET_DEC 4, ebx
-    add eax, ebx
-    PRINT_DEC 4, eax
-    ret
+    GET_DEC 4, eax ;считывается первое число,
+    cmp eax, 2     ; если оно равно 2 то осуществляется 
+    je binary      ; прыжок для работы с бинарным представлением
+    jne decimal    ; иначе (для 10-чных чисел) происходит простое сложение
+    decimal:
+        GET_DEC 4, eax 
+        GET_DEC 4, ebx
+        add eax, ebx
+        PRINT_DEC 4, eax ; вывод результата сложения в 10 представлении
+        ret
     
     
     
     binary:
-        call conv2Dec
-        mov [n1], esi
-        ;PRINT_DEC 4, [n1]
+        call conv2Dec ; прыжок к метке для перевода 2 числа в 10
+        mov [n1], esi ; поместить полученное 10-чное число в переменную n1
       
-        call conv2Dec
+        call conv2Dec ; аналогично как и выше только для второго числа
         mov [n2], esi
-        ;PRINT_DEC 4, [n2]
     
-        mov eax, [n1]
+        mov eax, [n1] ; перемещение в регистры чисел
         mov ebx, [n2]
-        add eax, ebx
-        call toBin
+        add eax, ebx ; их сложение
+        call toBin ; прыжок к метке для перевода числа из 10-чного представления в 2-чное
         xor eax, eax
         ret
              
@@ -46,41 +44,40 @@ CMAIN:
             GET_DEC 4, eax
             mov ecx, 4 ; размер 
             mov ebx, 10 ; делитель
-            l1:
+            l1:              ; деление в цикле и занесение остатка edx в стек
                 xor edx,edx
                 div ebx            
                 push edx
                 loop l1 
-            mov eax, 3  
+            ; по итогу в стеке бинарное представление числа по элементам
+            mov eax, 3  ; инициализация индекса для прохождения по массиву степеней 2 mult
             mov [i], eax
             xor esi, esi
-            toDec:
+            toDec: ; перевод числа из 2-чного в 10-чное в цикле
                 pop eax
                 mov ecx, [i]
-                mov ebx, [mult+ecx*4]
+                mov ebx, [mult+ecx*4] ; берется степень 2ки
                 mul ebx
-                add esi, eax
-                dec ecx
+                add esi, eax ; сложение полученнного результата
+                dec ecx ; декремент
                 mov [i], ecx
-                cmp ecx, 0
+                cmp ecx, 0 ; условие выхода из цикла
                 jge toDec
             ret
          
          
          toBin:
             mov ebp, esp
-            mov ecx, 4 ; размер 
+            mov ecx, 4 ; размер числа
             mov ebx, 2 ; делитель
             l2:
                 xor edx,edx
-                div ebx            
-                push edx
+                div ebx              ; деление числа на 2
+                push edx                ; занесение результата в стек
                 loop l2
             mov ecx, 4 ; размер 
-            l3:
-                pop eax
+            l3: ;вывод полученного числа из стека
+                pop eax 
                 PRINT_DEC 4, eax
                 loop l3
             ret
-    
-    
